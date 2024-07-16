0b670810e025dd6bf1530b755acaa7cdd8398ee5
Adolphus Miracle <miraboy13@gmail.com>
2024-07-16 06:34:50 +0100

    feat: created and implemented model for users table

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    //relationship
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
