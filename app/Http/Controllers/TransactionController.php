1fd06c40809b3bf16e698dc7bf1745ec9d8751d0
Adolphus Miracle <miraboy13@gmail.com>
2024-07-16 06:36:57 +0100

    feat: implemented functionality for transactions functionality

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\TransactionResource;
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    public function index()
+    {
+        return TransactionResource::collection(Transaction::all());
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'description' => 'nullable',
+            'amount' => 'required',
+        ]);
+        $transaction = Transaction::create($data);
+        return new TransactionResource($transaction);
+    }
+
+    public function show($id)
+    {
+        return new TransactionResource(Transaction::findOrFail($id));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'description' => 'nullable',
+            'amount' => 'required',
+        ]);
+        $transaction = Transaction::findOrFail($id);
+        $transaction->update($data);
+        return new TransactionResource($transaction);
+    }
+
+    public function destroy($id)
+    {
+        $transaction = Transaction::findOrFail($id);
+        $transaction->delete();
+        return response()->noContent();
+    }
+}
