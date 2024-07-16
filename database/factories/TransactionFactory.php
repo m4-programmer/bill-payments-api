ae0aead26b05d0dc4db1034df7118e13f1fbd16a
Adolphus Miracle <miraboy13@gmail.com>
2024-07-16 06:45:44 +0100

    feat: implemented test cases for transactions features

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Transaction>
+ */
+class TransactionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'description' => fake()->sentence,
+            'amount' => fake()->randomFloat(2, 1, 1000),
+        ];
+    }
+}
